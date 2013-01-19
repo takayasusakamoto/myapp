28d14d3bba46f54641611af771249ce5c0a58960
takayasu <takayasu@takayasu-VirtualBox.(none)>
2013-01-19 20:46:15 +0900

    addcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Myapp::Application.routes.draw do
-  resources :addresses
 
+  resources :addresses
+  resources :status, :only => [ :show] 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
